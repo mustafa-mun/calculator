e127c1961f949ca9e2576ee34f15b4c440098637
Mustafa <mustafamcm9@gmail.com>
2023-02-02 01:34:36 +0300

    multiple calculation feature is added

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const operations = document.getElementById('operations')
 
 const numberButtons = document.querySelectorAll('.number') // Number elements
 const operationButtons = document.querySelectorAll('.operation')
-const equalsButton = document.getElementById('equals')
+
+const clearButton = document.getElementById('clear');
+const acButton = document.getElementById('ac');
+const equalsButton = document.getElementById('equals');
+
 
 
 let arr = []
@@ -18,34 +22,68 @@ function operate(num1, operation, num2) {
   if(operation === '%') return num1 % num2
 }
 
-let t = 0 // Find a better way to do this (Deleting zero if any number button is pressed)
+
 numberButtons.forEach(item => {
 
   item.addEventListener('click', function(e) {
-    t++
-    if(t === 1) result.textContent = ''
+
+    if(result.textContent === '0' ) result.textContent = ''
     result.textContent += e.target.innerText
     num += e.target.innerText
+    console.log(num);
   })
 
 })
 
 operationButtons.forEach(item => {
   item.addEventListener('click', function(e) {
+    if(operation) { // If user try to do multiple calculations
+      pushResult()
+      } 
     operation = e.target.innerText
     arr.push(Number(num))
     num = ''
     result.textContent +=  ` ${e.target.innerText } `
+    console.log(arr);
   })
 })
 
 equalsButton.addEventListener('click', () => {
+  pushResult()
+})
+
+clearButton.addEventListener('click', () => {
+  deleteDigit()
+})
+
+acButton.addEventListener('click', () => {
+  clearAll()
+})
+
+
+function pushResult() {
   arr.push(Number(num))
+  console.log(num);
+  console.log(arr);
   result.textContent = `${operate(arr[0],operation,arr[1])}`
   arr = []
   operation = ''
   num = result.textContent
   console.log(num);
-})
+}
 
+function deleteDigit () {
+  const resultArr = result.textContent.split('')
+  const numArr = num.split('')
+  resultArr.pop()
+  numArr.pop()
+  num = numArr.join('');
+  result.textContent = resultArr.join('');
+}
 
+function clearAll () {
+  arr = []
+  operation = ''
+  num = '';
+  result.textContent = '0'
+}
